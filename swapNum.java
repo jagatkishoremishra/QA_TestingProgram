bff868e95ed366bb6c3eab49527876932d4f4450
jagatkishoremishra <jagatkishoremishra@hotmail.com>
2021-06-04 21:05:45 +0530

    Add files via upload

---
+++ swapNum.java
@@ -0,0 +1,15 @@
+package QA_Test;
+public class swapNum {
+
+	public static void main(String[] args) {
+		int x =23;
+		int y = 43;
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		
+		System.out.print(x);
+		System.out.print("\n"+y);
+	}
+}
